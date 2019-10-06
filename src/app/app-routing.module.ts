0ba6494e4b4b0d30b737926484baf44ced1f19e4
Miguel <ma.monreal@alumnos.urjc.es>
2019-10-06 18:47:00 +0200

    home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TeamComponent } from './team/team.component';
+import { HomeComponent } from './home/home.component';
+import { SprintComponent } from './sprint/sprint.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [ 
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'team',
+    component: TeamComponent
+  },
+  {
+    path: 'sprint',
+    component: SprintComponent
+  },
+  {
+    path: 'tutorial',
+    component: TutorialComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TeamComponent } from './team/team.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { SprintComponent } from './sprint/sprint.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamComponent,
+    NavBarComponent,
+    HomeComponent,
+    SprintComponent,
+    TutorialComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
